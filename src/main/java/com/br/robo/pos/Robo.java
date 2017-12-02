37cdad8f8b83b9baf651cdf68689e438eb137f14
Leandro Vitor <vitoleandro@gmail.com>
2017-12-02 15:09:40 -0300

    Movimentando para x e y

--- src/main/java/com/br/robo/pos/Robo.java
+++ src/main/java/com/br/robo/pos/Robo.java
@@ -1,7 +1,5 @@
 package com.br.robo.pos;
 
-import java.util.Arrays;
-
 public class Robo {
 	private int[][] plano;
 
@@ -25,7 +23,7 @@ public class Robo {
 	public int getPosition() {
 		return plano[this.y][this.x];
 	}
-	
+
 	public void clear() {
 		for (int y = 0; y < this.plano.length; y++) {
 			for (int x = 0; x < this.plano[y].length; x++) {
@@ -33,20 +31,53 @@ public class Robo {
 			}
 		}
 	}
-	
+
 	private void setPosition() {
 		this.clear();
 		this.plano[this.y][this.x] = 1;
 	}
 
-	public int moveY() {
-		this.y++;
-		this.setPosition();
+	public int moveY(boolean down) {
+		if (down && this.canDown()) {
+			this.y++;
+			this.setPosition();
+		} else {
+			// move pra cima
+			if (this.canUp()) {
+				this.y--;
+				this.setPosition();
+			}
+		}
+
 		return this.y;
 	}
 
-	@Override
-	public String toString() {
-		return "X: " + x + " Y: " + y;
+	public int moveX(boolean right) {
+		if (right && this.canRight()) {
+			this.x++;
+			this.setPosition();
+		} else {
+			if(this.canLeft()) {
+				this.x--;
+				this.setPosition();
+			}
+		}
+		return this.x;
+	}
+
+	private boolean canUp() {
+		return (this.y > 0);
+	}
+
+	private boolean canRight() {
+		return this.x + 1 < this.limit;
+	}
+
+	private boolean canLeft() {
+		return (this.x > 0);
+	}
+
+	private boolean canDown() {
+		return (this.y + 1) < this.limit;
 	}
 }
--- src/test/java/com/br/robo/pos/RoboTest.java
+++ src/test/java/com/br/robo/pos/RoboTest.java
@@ -7,24 +7,92 @@ import org.junit.Test;
 
 public class RoboTest {
 	private Robo robo;
-	
+
 	@Before
 	public void setup() {
 		this.robo = new Robo();
 	}
-	
+
 	@Test
 	public void deveTerPosicaoInicialZero() {
 		assertEquals(robo.getX(), 0);
 		assertEquals(robo.getY(), 0);
 		assertEquals(robo.getPosition(), 1);
 	}
-	
+
 	@Test
 	public void deveAndarParaTrasAPartirDaPosicaoInicial() {
-		robo.moveY();
+		robo.moveY(true);
 		assertEquals(robo.getY(), 1);
 		assertEquals(robo.getX(), 0);
 		assertEquals(robo.getPosition(), 1);
+	}
+
+	@Test
+	public void naoDeveAndarMaisQueOlimiteParaBaixo() {
+		// Executa mais que o limite
+		for (int i = 0; i < 5; i++) {
+			robo.moveY(true);
+		}
+
+		assertEquals(robo.getY(), 3);
+		assertEquals(robo.getX(), 0);
+		assertEquals(robo.getPosition(), 1);
+	}
+
+	@Test
+	public void naoDeveAndarParaBaixoQuandoForZero() {
+		robo.moveY(false);
+		assertEquals(robo.getY(), 0);
+		assertEquals(robo.getX(), 0);
+		assertEquals(robo.getPosition(), 1);
+	}
+
+	@Test
+	public void deveAndarParaBaixoQuandoForMaiorQueZero() {
+		// anda duas uma pra baixo
+		robo.moveY(true);
+		robo.moveY(true);
+
+		// anda pra cima
+		robo.moveY(false);
+
+		assertEquals(robo.getY(), 1);
+		assertEquals(robo.getX(), 0);
+		assertEquals(robo.getPosition(), 1);
+	}
+
+	@Test
+	public void deveAndarParaADireita() {
+		robo.moveX(true);
+		assertEquals(robo.getX(), 1);
+	}
+
+	@Test
+	public void naoDeveAndarMaisQueOLimiteParaADireita() {
+
+		for (int i = 0; i < 5; i++) {
+			robo.moveX(true);
+		}
+
+		assertEquals(robo.getX(), 3);
+	}
+
+	@Test
+	public void deveAndarParaAEsquerda() {
+		robo.moveX(true);
+		robo.moveX(true);
+		robo.moveX(false);
+		assertEquals(robo.getX(), 1);
+	}
+
+	@Test
+	public void naoDeveAndarMaisQueOLimiteParaAEsquerda() {
+
+		for (int i = 0; i < 5; i++) {
+			robo.moveX(false);
+		}
+
+		assertEquals(robo.getX(), 0);
 	}
 }
